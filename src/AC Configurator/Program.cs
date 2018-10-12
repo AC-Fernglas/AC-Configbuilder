5b9f25c5f77f6a8edbb3ad916aa0918dfce503b6
Zeroks77 <dominik@mangatter.de>
2018-10-12 08:19:56 +0200

    01.02c

--- src/AC Configurator/Program.cs
+++ src/AC Configurator/Program.cs
@@ -9,45 +9,70 @@ namespace AC_Configurator
     {
         public static void  Main(string[] args)
         {
-            jsoncoverter.getjson();
+            jsonconstructor.getjson();
+            jsonconstructor.constructjson();
         }
     }
     class jsoncoverter
     {
-        public static void getjson()
-        {
-            string json = JsonConvert.SerializeObject(File.ReadAllText(@"C:\Develop\AC-Configbuilder\config\Konfigurationen.json"), Formatting.Indented);
-            Console.WriteLine(json);
+       
 
-            jsonconstructor Baum = new jsonconstructor
-            {
-                ALTER = 12,
-                HÖHE = 1234,
-                GEWICHT = 23476,
-                ART = "Eiche"
-
-            };
-
-          string myjson = JsonConvert.SerializeObject(Baum, Formatting.Indented);
-            Console.ReadLine();
-        }
-
+      
     }
 
-    class jsonconstructor
+    class jsonconstructor  // funktioniert
     {
         public int  ALTER { get; set; }
         public int HÖHE { get; set; }
         public int GEWICHT { get; set; }
         public string ART { get; set; }
 
-        public void construct(int age, int height, int weight, string species)
+        private void construct(int age, int height, int weight, string species)
         {
             ALTER = age;
             HÖHE = height;
             GEWICHT = weight;
             ART = species;
 
-        } 
+        }
+
+        public static void constructjson()
+        {
+            jsonconstructor Baum = new jsonconstructor
+            {
+                ALTER = 12,
+                HÖHE = 1234,
+                GEWICHT = 23476,
+                ART = "Eiche"
+            };
+
+            string myjson = JsonConvert.SerializeObject(Baum, Formatting.Indented);
+            Console.WriteLine(myjson);
+            setjson(Baum);
+            Console.ReadLine();
+        }
+
+        public static void getjson()
+        {
+            jsonconstructor Baum1 = new jsonconstructor();
+            using (StreamReader file = File.OpenText(@"..\..\..\..\config\Konfigurationen.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                
+                Baum1 = (jsonconstructor)serializer.Deserialize(file, typeof(jsonconstructor));
+            }
+            Console.WriteLine(Baum1.ALTER);
+            setjson(Baum1);
+            Console.ReadLine();
+        }
+
+        public static void setjson(jsonconstructor myjson)
+        {
+            using (StreamWriter file = File.CreateText(@"..\..\..\..\config\Konfigurationen.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, myjson);
+            }
+        }
     }
 }
