09c2e03570d0401cf64cba0ac0433be8a02dbd8f
Zeroks77 <dominik@mangatter.de>
2019-05-21 11:09:49 +0200

    improved main programm & simplified it

---
+++ src/InputToACObject.cs
@@ -0,0 +1,336 @@
+﻿using Sprache;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ACConfigBuilder
+{
+    public class InputToACObject
+    {
+        public void getIdentNameAndValue(string line, out bool configureExit, out bool subIdentExit, out string subIdent, out string subIdentValue) // parses the Indentifyer for the current block of the configuration
+        {
+            subIdent = ParserGrammar.getsubident.Parse(line);
+            configureExit = false;
+            subIdentExit = false;
+            subIdentValue = String.Empty;
+            if (subIdent == ParserVariables.networkDev ||
+                subIdent == ParserVariables.interfaceNetwokIf ||
+                subIdent == ParserVariables.proxySet ||
+                subIdent == ParserVariables.proxyIp ||
+                subIdent == ParserVariables.exit)
+            {
+                if (subIdent == ParserVariables.exit)
+                {
+                    configureExit = true;
+                    subIdentExit = true;
+                    return;
+                }
+                subIdentExit = false;
+                subIdentValue = ParserGrammar.subidentvalue.Parse(line);
+            }
+        }
+        public void getConfigureIdent(string line, out bool configureExit, out string ident) //parses the head of the currend configurationblock
+        {
+            ident = String.Empty;
+            configureExit = true;
+            var parsedLine = ParserGrammar.getidentifier.Parse(line);
+            if (parsedLine == "configure network" || parsedLine == "configure voip" || parsedLine == Environment.NewLine || parsedLine == "\n")
+            {
+                ident = parsedLine;
+                configureExit = false;
+            }
+        }
+        public ACConfig parseinobject(StreamReader Reader)
+        {
+            Configureviop vo = new Configureviop();
+            ConfigureNetwork co = new ConfigureNetwork();
+            List<Networkdev> networkDevs = new List<Networkdev>();
+            List<Interfacenetworkif> interfaceNetworkIfs = new List<Interfacenetworkif>();
+            List<Proxyip> proxyIps = new List<Proxyip>();
+            List<Proxyset> proxySets = new List<Proxyset>();
+            ACConfig AC = new ACConfig();
+
+
+            AC.configureNetwork = co;
+            AC.configureviop = vo;
+
+            AC.configureNetwork.networkdev = networkDevs;
+            AC.configureNetwork.interfacenetworkif = interfaceNetworkIfs;
+
+            AC.configureviop.proxyip = proxyIps;
+            AC.configureviop.proxyset = proxySets;
+
+            string ident = String.Empty;
+            bool configureExit = true;
+            string subIdent = String.Empty;
+            bool subIdentExit = true;
+            string subIdentValue = String.Empty;
+            int networkDevListTndex = 0;
+            int interfaceNetwokIfListTndex = 0;
+            int proxySetListTndex = 0;
+            int proxyIpListTndex = 0;
+
+            using (Reader)
+            {
+                string line = " ";
+                while ((line = Reader.ReadLine()) != null)                                                  // zu editierende File einlesen (Zeile für Zeile)
+                {
+                    if (line == string.Empty || line == "")                                                 // wenn leere Zeile überspringe
+                    {
+                        continue;
+                    }
+                    if (configureExit)                                                                      // wenn True -> kein Überbereich(configure voip // configure network) in dem die Konfig definiert wird
+                    {
+                        getConfigureIdent(line, out configureExit, out ident);
+                        continue;
+                    }
+                    if (subIdentExit)
+                    {
+                        getIdentNameAndValue(line, out configureExit, out subIdentExit, out subIdent, out subIdentValue);                   // wenn true -> keien Bereich(networkDev / interfaceNetworkIf) in dem Konfig konfiguriert wird
+                        var Name = returnRealName(subIdent);
+                        if (Name == null)
+                        {
+                            continue;
+                        }
+                        if (subIdent == ParserVariables.networkDev)
+                        {
+                            Networkdev newListNetworkDev = new Networkdev();
+                            networkDevs.Add(newListNetworkDev);                                                                           //neue Liste wird erstellt für jede Bereich von networkDev
+                            AC = ListParsing(AC, Name, subIdentValue, networkDevListTndex, AC.configureNetwork.networkdev, subIdent);
+                        }
+                        else if (subIdent == ParserVariables.interfaceNetwokIf)
+                        {
+                            Interfacenetworkif newListInterfaceNetworkIf = new Interfacenetworkif();
+                            interfaceNetworkIfs.Add(newListInterfaceNetworkIf);
+                            AC = ListParsing(AC, Name, subIdentValue, interfaceNetwokIfListTndex, AC.configureNetwork.interfacenetworkif, subIdent);
+                        }
+                        else if (subIdent == ParserVariables.proxySet)
+                        {
+                            Proxyset newListProxySet = new Proxyset();
+                            proxySets.Add(newListProxySet);
+                            AC = ListParsing(AC, Name, subIdentValue, proxySetListTndex, AC.configureviop.proxyset, subIdent);
+                        }
+                        else if (subIdent == ParserVariables.proxyIp)
+                        {
+                            Proxyip newListProxyIp = new Proxyip();
+                            proxyIps.Add(newListProxyIp);
+                            AC = ListParsing(AC, Name, subIdentValue, proxyIpListTndex, AC.configureviop.proxyip, subIdent);
+                        }
+                        continue;
+                    }
+
+                    if (ident == "configure network" && subIdent == ParserVariables.networkDev)
+                    {
+                        var Name = ParserGrammar.NameParser.Parse(line);
+                        if (Name == ParserVariables.exit) // Bereich wird durch exit beendet -> Listenindex wird erhöht und es gibt keinen Bereich in dem definiert die Konfig defoiniert wird
+                        {
+                            subIdentExit = true;
+                            networkDevListTndex++;
+                            continue;
+                        }
+                        Name = returnRealName(Name);
+                        if (Name == null)
+                        {
+                            continue;
+                        }
+                        AC = AddToList(AC, Name, AC.configureNetwork.networkdev, subIdent, networkDevListTndex, ParserGrammar.ValueParser.Parse(line));
+                        continue;
+                    }
+                    else if ( ident == "configure network" && subIdent == ParserVariables.interfaceNetwokIf)
+                    {
+                        var Name = ParserGrammar.NameParser.Parse(line);
+                        if (Name == ParserVariables.exit)
+                        {
+                            subIdentExit = true;
+                            interfaceNetwokIfListTndex++;
+                            continue;
+                        }
+                        Name = returnRealName(Name);
+                        if (Name == null)
+                        {
+                            continue;
+                        }
+                        AC = AddToList(AC, Name, AC.configureNetwork.interfacenetworkif, subIdent, interfaceNetwokIfListTndex, ParserGrammar.ValueParser.Parse(line));
+                        continue;
+                    }
+                    if (ident == "configure voip" && subIdent == ParserVariables.proxySet)
+                    {
+                        var Name = ParserGrammar.NameParser.Parse(line);
+                        if (Name == ParserVariables.exit)
+                        {
+                            subIdentExit = true;
+                            proxySetListTndex++;
+                            continue;
+                        }
+                        Name = returnRealName(Name);
+                        if (Name == null)
+                        {
+                            continue;
+                        }
+                        AC = AddToList(AC, Name, AC.configureviop.proxyset, subIdent, proxySetListTndex, ParserGrammar.ValueParser.Parse(line));
+                        continue;
+                    }
+                    else if (ident == "configure voip" && subIdent == ParserVariables.proxyIp)
+                    {
+                        var Name = ParserGrammar.NameParser.Parse(line);
+                        if (Name == ParserVariables.exit)
+                        {
+                            subIdentExit = true;
+                            proxyIpListTndex++;
+                            continue;
+                        }
+                        Name = returnRealName(Name);
+                        if (Name == null)
+                        {
+                            continue;
+                        }
+                        AC = AddToList(AC,Name, AC.configureviop.proxyip,subIdent,proxyIpListTndex, ParserGrammar.ValueParser.Parse(line));
+                        continue;
+                    }
+                }
+            }
+            return AC;
+        }
+        public ACConfig AddToList(ACConfig AC, string Name, dynamic List, string subIdent,int Index, string value )
+        {
+            if (Name == ParserVariables.activate)
+            {
+                AC = ListParsing(AC, Name, true, Index, List, subIdent);
+            }
+            else
+            {
+                AC = ListParsing(AC, Name, value, Index, List, subIdent);
+            }
+            return AC;
+        }
+        public string returnRealName(string Name) // wandelt die in der Konfiguration enthaltenen bezeichner in die Variablen Namen der Listen um da diese nicht zu 100% übereinstimmen 
+        {
+            switch (Name)
+            {
+                case ParserVariables.proxySet:
+                case ParserVariables.networkDev:
+                case ParserVariables.interfaceNetwokIf:
+                    return ParserVariables.listid;
+                case ParserVariables.proxyIp:
+                    return ParserVariables.ip;
+                case ParserVariables.activate:
+                    return ParserVariables.activate;
+                case ParserVariables.vlanid:
+                    return ParserVariables.vlan;
+                case ParserVariables.underlyingdev:
+                    return ParserVariables.udev;
+                case ParserVariables.Name:
+                    return ParserVariables.name;
+                case ParserVariables.tagging:
+                    return ParserVariables.Tag;
+                case ParserVariables.apptype:
+                    return ParserVariables.appt;
+                case ParserVariables.ipaddress:
+                    return ParserVariables.ipa;
+                case ParserVariables.prefixlength:
+                    return ParserVariables.prefix;
+                case ParserVariables.gateway:
+                    return ParserVariables.gate;
+                case ParserVariables.underlyingif:
+                    return ParserVariables.uif;
+                case ParserVariables.proxyname:
+                    return ParserVariables.pname;
+                case ParserVariables.proxyenablekeepalive:
+                    return ParserVariables.proxyalive;
+                case ParserVariables.srdname:
+                    return ParserVariables.sname;
+                case ParserVariables.sbcipv4sipintname:
+                    return ParserVariables.sbcipv;
+                case ParserVariables.keepalivefailresp:
+                    return ParserVariables.keepresp;
+                case ParserVariables.successdetectretries:
+                    return ParserVariables.successdet;
+                case ParserVariables.successdetectint:
+                    return ParserVariables.successdetint;
+                case ParserVariables.proxyredundancymode:
+                    return ParserVariables.proxymode;
+                case ParserVariables.isproxyhotswap:
+                    return ParserVariables.proxyswap;
+                case ParserVariables.proxyloadbalancingmethod:
+                    return ParserVariables.proxymethod;
+                case ParserVariables.minactiveservlb:
+                    return ParserVariables.minlb;
+                case ParserVariables.proxyaddress:
+                    return ParserVariables.padress;
+                case ParserVariables.transporttype:
+                    return ParserVariables.ttype;
+                default:
+                    return null;
+            }
+        }
+        protected ACConfig ListParsing(ACConfig Config, string Name, dynamic Value, int Index, dynamic myList, string subIdent) //setzt die Value in aus dfem Bereich in die Passende liste an der Richtigen stelle
+        {
+            if (String.IsNullOrWhiteSpace(Value.ToString())) // falls es keine Value gibt brauch es nix machen
+            {
+                return Config;
+            }
+            foreach (var item in myList[Index].GetType().GetProperties()) //property aus index X   bekommeb
+            {
+                var property = item;
+                if (property.Name != Name) // wenn die property nicht den selben bezeichner hat wie der gegebene Name
+                {
+                    continue;
+                }
+                var propertyType = property.PropertyType; // Gets the type of the property
+
+                if (propertyType == typeof(Int32) || propertyType == typeof(Nullable<Int32>)) //if type = int then parse Value into an integer
+                {
+                    property.SetValue(myList[Index], Convert.ToInt32(Value));
+                }
+                else if (propertyType.IsEnum) // if type = enum then parse Value into a enum
+                {
+                    var enumMember = propertyType
+                        .GetFields(); // List all fields of an enum
+                    foreach (var member in enumMember)
+                    {
+                        if (member.Name == Convert.ToString(Value) ||
+                             Convert.ToString(Value) == Convert.ToString(member.GetCustomAttributes(typeof(NameAttribute), false))) // if the name of the field = Name or the Attribute = Name 
+                        {
+                            if (member == null)
+                            {
+                                Console.WriteLine($"Warn: skip property {property.Name} because the value {Value} is not valid for this field.");
+                                return Config;
+                            }
+                            var enumValue = Enum.Parse(propertyType, member.Name);  // parses Value into an enumValue
+                            property.SetValue(myList[Index], enumValue);
+
+                        }  //Console.WriteLine($"Warn: skip property {property.Name} because enums currently not supported.");
+                    }
+                }
+                else if (propertyType == typeof(Boolean)) // if type = boolean then parse Value into a boolean
+                {
+                    property.SetValue(myList[Index], Convert.ToBoolean(Value));
+                }
+                else                            //if type is something else (hopefully a string) then replace the old value with the new
+                {
+                    property.SetValue(myList[Index], Value);
+                }
+                switch (subIdent) // returns the right Config 
+                {
+                    case ParserVariables.networkDev:
+                        Config.configureNetwork.networkdev = myList;
+                        return Config;
+                    case ParserVariables.interfaceNetwokIf:
+                        Config.configureNetwork.interfacenetworkif = myList;
+                        return Config;
+                    case ParserVariables.proxySet:
+                        Config.configureviop.proxyset = myList;
+                        return Config;
+                    case ParserVariables.proxyIp:
+                        Config.configureviop.proxyip = myList;
+                        return Config;
+                    default:
+                        break;
+                }
+                return Config;
+            }
+            return Config;
+        }
+    }
+}
--- test/OutputTest/MainProgrammTest.cs
+++ test/OutputTest/MainProgrammTest.cs
@@ -16,175 +16,175 @@ namespace Tests
         public void ReturnTestRealNameListID()
         {
             string TestName = "network-dev";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("listid", TestName);
         }
         [Fact]
         public void ReturnTestRealNameIP()
         {
             string TestName = "proxy-ip";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("ip", TestName);
         }
         [Fact]
         public void ReturnTestRealNameActivateVlanID()
         {
             string TestName = "activate";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("activate", TestName);
         }
         [Fact]
         public void ReturnTestRealNameVlanID()
         {
             string TestName = "vlan-id";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("vlan", TestName);
         }
         [Fact]
         public void ReturnTestRealNameUnderlyingIf()
         {
             string TestName = "underlying-if";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("underlyingif", TestName);
         }
         [Fact]
         public void ReturnTestRealNameUnderlyingDev()
         {
             string TestName = "underlying-dev";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("underlyingdev", TestName);
         }
         [Fact]
         public void ReturnTestRealNameName()
         {
             string TestName = "name";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("Name", TestName);
         }
         [Fact]
         public void ReturnTestRealNameTagging()
         {
             string TestName = "tagging";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("tag", TestName);
         }
         [Fact]
         public void ReturnTestRealNameApptype()
         {
             string TestName = "application-type";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("apptype", TestName);
         }
         [Fact]
         public void ReturnTestRealNameIPAdress()
         {
             string TestName = "ip-address";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("ipaddress", TestName);
         }
         [Fact]
         public void ReturnTestRealNameGateway()
         {
             string TestName = "gateway";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("gateway", TestName);
         }
         [Fact]
         public void ReturnTestRealNameProxyName()
         {
             string TestName = "proxy-name";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("proxyname", TestName);
         }
         [Fact]
         public void ReturnTestRealNameSrdName()
         {
             string TestName = "srd-name";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("srdname", TestName);
         }
         [Fact]
         public void ReturnTestRealNameProxyEnablekeepAlive()
         {
             string TestName = "proxy-enable-keep-alive";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("proxyenablekeepalive", TestName);
         }
         [Fact]
         public void ReturnTestRealNameSPCIV4()
         {
             string TestName = "sbcipv4-sip-int-name";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("sbcipv4sipintname", TestName);
         }
         [Fact]
         public void ReturnTestRealNameKeepAliveFailResp()
         {
             string TestName = "keepalive-fail-resp";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("keepalivefailresp", TestName);
         }
         [Fact]
         public void ReturnTestRealNameSuccessRetries()
         {
             string TestName = "success-detect-retries";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("successdetectretries", TestName);
         }
         [Fact]
         public void ReturnTestRealNameSuccessint()
         {
             string TestName = "success-detect-int";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("successdetectint", TestName);
         }
         [Fact]
         public void ReturnTestRealNameProxyRedunadacyMode()
         {
             string TestName = "proxy-redundancy-mode";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("proxyredundancymode", TestName);
         }
         [Fact]
         public void ReturnTestRealNameHotSwap()
         {
             string TestName = "is-proxy-hot-swap";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("isproxyhotswap", TestName);
         }
         [Fact]
         public void ReturnTestRealNameProxyBalancingMethode()
         {
             string TestName = "proxy-load-balancing-method";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("proxyloadbalancingmethod", TestName);
         }
         [Fact]
         public void ReturnTestRealNameMinActive()
         {
             string TestName = "min-active-serv-lb";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("minactiveservlb", TestName);
         }
         [Fact]
         public void ReturnTestRealNameProxyAddress()
         {
             string TestName = "proxy-address";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("proxyaddress", TestName);
         }
         [Fact]
         public void ReturnTestRealNameTransportType()
         {
             string TestName = "transport-type";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Contains("transporttype", TestName);
         }
         [Fact]
         public void ReturnTestRealNameDefault()
         {
             string TestName = "blub";
-            TestName = new TestMainProgramm().ReturnTestName(TestName);
+            TestName = new Input2Object().ReturnTestName(TestName);
             Assert.Null(TestName);
         }
         [Fact]
@@ -244,7 +244,7 @@ namespace Tests
                 "exit"
             };
 
-            ACConfig TestAC = new TestMainProgramm().ParseInTestObject(new StreamReader(GenerateStreamFromList(FakeConfig)));
+            ACConfig TestAC = new Input2Object().ParseInTestObject(new StreamReader(GenerateStreamFromList(FakeConfig)));
             Assert.True(TestAC.configureNetwork.networkdev[0].tag.ToString() == "untagged");
             Assert.True(TestAC.configureNetwork.networkdev[0].activate == true);
             Assert.True(TestAC.configureNetwork.interfacenetworkif[0].Name == "blab");
@@ -382,7 +382,6 @@ namespace Tests
                 configureNetwork = new ConfigureNetwork()
                 {
                     networkdev = new List<Networkdev>()
-
                 }
             };
             Networkdev Nd = new Networkdev()
@@ -403,33 +402,32 @@ namespace Tests
             Assert.Equal(0,Empty);
         }
     }
-    public class TestMainProgramm : Execute
+    public class Input2Object : InputToACObject
     {
         public string ReturnTestName(string TestName)
         {
             return returnRealName(TestName);
         }
-        public string ValidTestPath(string OtherPath, string Path)
-        {
-            return validpath(Path, OtherPath);
-        }
-        public List<string> FindTestDirectory(string path)
-        {
-            return findFilesInDirectory(path);
-        }
         public ACConfig ParseInTestObject(StreamReader Reader)
         {
             return parseinobject(Reader);
         }
-
-        public ACConfig ReplaceTestValues(ACConfig AC, dynamic list, string whatlist)
-        {
-            return replaceitem(AC, list, whatlist);
-        }
-        
-
     }
-
+    public class TestMainProgramm : Execute
+        {
+            public ACConfig ReplaceTestValues(ACConfig AC, dynamic list, string whatlist)
+            {
+                return replaceitem(AC, list, whatlist);
+            }
+            public string ValidTestPath(string OtherPath, string Path)
+            {
+                return validpath(Path, OtherPath);
+            }
+            public List<string> FindTestDirectory(string path)
+            {
+                return findFilesInDirectory(path);
+            }
+        }
     public class TestCommands : Commands
     {
         public int TestIdel(string[] commands)
